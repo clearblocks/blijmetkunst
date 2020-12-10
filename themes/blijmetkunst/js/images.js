c6be0dcb6f1a964241d60017902f989c1c6a9cf4
Theo van Oostrum <theo@xel.nl>
2020-12-10 18:59:29 +0100

    image modal and style changes

--- themes/blijmetkunst/js/images.js
+++ themes/blijmetkunst/js/images.js
@@ -3,35 +3,14 @@ var BmkImages = (function() {
     var self = this;
 
     object.initialize = function() {
-        var images = document.querySelectorAll('[data-fig-img]');
+        var figurineImages = document.querySelectorAll('[data-fig-img]');
+        var galleryImages = document.querySelectorAll('.blocks-gallery-item img');
+
+        object.setImageClickEvents(figurineImages);
+        object.setImageClickEvents(galleryImages);
+
+
         var imageModal = document.querySelector('#image-modal');
-
-        for (key in images) {
-            if (images.hasOwnProperty(key)) {
-                var image = images[key];
-
-                image.onload = function() {
-                    if (this.naturalWidth > this.naturalHeight) {
-                        this.parentNode.classList.add('image-landscape');
-                    }
-                }
-
-                image.onclick = function (event) {
-                    var currentImage = event.target;
-                    var url = currentImage.getAttribute('src');
-                    var img = document.createElement('img');
-                    img.setAttribute('src', url);
-                    img.onclick = function (event) {
-                        event.stopPropagation();
-                    };
-                    imageModal.querySelector('.modal-body').innerHTML = '';
-                    imageModal.querySelector('.modal-body').appendChild(img);
-                    imageModal.classList.add("open");
-                    document.body.classList.add("no-scroll");
-                };
-            }
-        }
-
         imageModal.querySelector('.modal-body').onclick = function (event) {
             event.stopPropagation();
             imageModal.classList.remove('open');
@@ -49,6 +28,39 @@ var BmkImages = (function() {
             imageModal.classList.remove('open');
             document.body.classList.remove("no-scroll");
         };
+    }
+
+    object.setImageClickEvents = function(images) {
+        for (key in images) {
+            if (images.hasOwnProperty(key)) {
+                var image = images[key];
+
+                image.onload = function() {
+                    if (this.naturalWidth > this.naturalHeight) {
+                        this.parentNode.classList.add('image-landscape');
+                    }
+                }
+
+                image.onclick = function (event) {
+                    var currentImage = event.target;
+                    var url = currentImage.getAttribute('src');
+                    var img = document.createElement('img');
+                    img.setAttribute('src', url);
+
+                    if (this.naturalWidth > this.naturalHeight) {
+                        img.classList.add('image-landscape');
+                    }
+                    img.onclick = function (event) {
+                        event.stopPropagation();
+                    };
+                    var imageModal = document.querySelector('#image-modal');
+                    imageModal.querySelector('.modal-body').innerHTML = '';
+                    imageModal.querySelector('.modal-body').appendChild(img);
+                    imageModal.classList.add("open");
+                    document.body.classList.add("no-scroll");
+                };
+            }
+        }
     }
 
     return object;
